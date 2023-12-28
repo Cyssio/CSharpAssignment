f9ea713f67401dce07f0127479f915e496b180c5
Mattias <mattias.kasto@gmail.com>
2023-12-28 15:54:49 +0100

    Add project files.

---
+++ Assignment.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Assignment.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ AssignmentShared/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace AssignmentShared
+{
+    public class Class1
+    {
+
+    }
+}
